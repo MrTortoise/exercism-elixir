692ee6022b36ef6309a7f6e0da6ee80bd79cd813
john nicholas <john.nicholas@dinoroar.io>
2021-02-22 16:58:53 +0000

    word count done

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -4,16 +4,37 @@ defmodule WordCount do
 
   Words are compared case-insensitively.
   """
+
+  @punctuation "!&@$%^&:,"
+
   @spec count(String.t()) :: map
   def count(sentence) do
-    String.split(sentence)
+    sentence
+    |> String.split([" ", "_"])
+    |> Enum.map(&strip_punctuation/1)
+    |> Enum.reject(&is_empty/1)
+    |> Enum.map(&String.downcase/1)
     |> calculate_frequencies()
   end
 
+  #felt like Enum.frequencies was probably 'cheating' here
   defp calculate_frequencies(string_list) do
     string_list
-    |> Enum.reduce(%{}, fn (s, acc) ->
-      Map.update(acc, s, 1, fn existing -> existing+1 end)
+    |> Enum.reduce(%{}, fn s, acc ->
+      Map.update(acc, s, 1, fn existing -> existing + 1 end)
     end)
   end
+
+  defp strip_punctuation(chars) do
+    chars
+    |> String.graphemes()
+    |> Enum.reject(&is_punctuation/1)
+    |> Enum.join()
+  end
+
+  defp is_punctuation(char) do
+    String.contains?(@punctuation, char)
+  end
+
+  defp is_empty(s), do: s == ""
 end
